1c0adefb6bd5303803ed1b4aaa56eb6fea21e12f
Camila Rodrigues <camila.not.camih@hotmail.com>
2022-01-17 09:15:24 -0300

    Initial Commit

---
+++ AdviceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdviceApp
+//
+//  Created by Camila Ribeiro Rodrigues on 17/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
